0c17e07090ac8fd23b249e388f0508adefd5c0e9
cyborg <skymaya@gmail.com>
2017-06-02 16:15:28 -0400

    function spacing

--- unitconvert/runconvert.py
+++ unitconvert/runconvert.py
@@ -18,7 +18,6 @@ TIME_UNITS = ['ms', 'sec', 'wk', 'day', 'hr', 'min', 'mo', 'yr']
 DIGITAL_UNITS = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
 LENGTH_UNITS = ['mm', 'cm', 'in', 'ft', 'yd', 'm', 'km', 'mi']
 
-
 def do_argparser():
     """Parse and return command line arguments"""
     parser = argparse.ArgumentParser(
@@ -36,14 +35,12 @@ def do_argparser():
     parser.add_argument('-t', '--unit_to', help='Unit to convert to')
     return parser.parse_args()
 
-
 def doconvert(ufrom, uto, amount, init):
     """Given a unit from, unit to convert to, amount to convert, and an
     instance that calculates the units' base value, return the converted
     amount"""
     conversion = (float(amount) * init.getunitval(ufrom)) / init.getunitval(uto)
     return "{0} {1} is {2} {3}".format(amount, ufrom, conversion, uto)
-
 
 def main():
     """Main function"""
@@ -60,7 +57,6 @@ def main():
     if args.unit_from in TIME_UNITS and args.unit_to in TIME_UNITS:
         timeu = TimeUnit()
         print(doconvert(args.unit_from, args.unit_to, args.amount, timeu))
-
 
 if __name__ == "__main__":
     main()
