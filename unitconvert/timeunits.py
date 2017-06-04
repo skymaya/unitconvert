b212ec513b7bc8c19ac305d6536317f0387709a4
cyborg <skymaya@gmail.com>
2017-06-04 01:41:44 -0400

    calculate based on minutes

--- unitconvert/timeunits.py
+++ unitconvert/timeunits.py
@@ -3,7 +3,7 @@
 class TimeUnit(object):
     """placeholder docstring"""
     def __init__(self):
-        self.time_base = 1.0
+        self.time_base = 1.0  # minutes in a minute
 
     def getunitval(self, argument):
         """Return a function to calculate the unit's value"""
@@ -14,39 +14,39 @@ class TimeUnit(object):
     def unit_ms(self):
         """Return the value of one Millisecond (ms)
         based on a base time value"""
-        return self.time_base
+        return (self.time_base / 1000.0) / 60.0
 
     def unit_sec(self):
         """Return the value of one Second (sec)
         based on a base time value"""
-        return self.time_base * 1000.0
+        return self.time_base / 60.0
 
     def unit_min(self):
         """Return the value of one Minute (min)
         based on a base time value"""
-        return (self.time_base * 1000.0) * 60.0
+        return self.time_base
 
     def unit_hr(self):
         """Return the value of one Hour (hr)
         based on a base time value"""
-        return (self.time_base * 1000.0) * 3600.0
+        return self.time_base * 60.0
 
     def unit_day(self):
         """Return the value of one Day (day)
         based on a base time value"""
-        return ((self.time_base * 1000.0) * 3600.0) * 24.0
+        return (self.time_base * 60.0) * 24.0
 
     def unit_wk(self):
         """Return the value of one Week (wk)
         based on a base time value"""
-        return (((self.time_base * 1000) * 3600.0) * 24.0) * 7
+        return ((self.time_base * 60.0) * 24.0) * 7
 
     def unit_mo(self):
         """Return the value of one Month (mo)
         based on a base time value"""
-        return (((self.time_base * 1000) * 3600.0) * 365.0) / 12
+        return (((self.time_base * 60.0) * 24.0) * 365.0) / 12
 
     def unit_yr(self):
         """Return the value of one Year (yr)
         based on a base time value"""
-        return (((self.time_base * 1000) * 3600.0) * 24.0) * 365.0
+        return ((self.time_base * 60.0) * 24.0) * 365.0
