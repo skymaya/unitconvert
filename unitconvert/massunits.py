4dabd9a8b4585a2cca88cb6084b50640f015c49f
cyborg <skymaya@gmail.com>
2017-06-04 01:30:09 -0400

    base on grams per ounce

--- unitconvert/massunits.py
+++ unitconvert/massunits.py
@@ -4,8 +4,8 @@
 class MassUnit(object):
     """placeholder docstring"""
     def __init__(self):
-        self.metric_base = 1.0
-        self.imperial_base = 28349.5231
+        self.metric_base = 1.0  # grams in a gram
+        self.imperial_base = 28.349523125  # grams in an ounce
 
     def getunitval(self, argument):
         """Return a function to calculate the unit's value"""
@@ -16,12 +16,12 @@ class MassUnit(object):
     def unit_mg(self):
         """Return the value of one Milligram (mg)
         based on a base metric value"""
-        return self.metric_base
+        return self.metric_base / 1000.0
 
     def unit_g(self):
         """Return the value of one Gram (g)
         based on a base metric value"""
-        return self.metric_base * 1000.0
+        return self.metric_base
 
     def unit_oz(self):
         """Return the value of one Ounce (oz)
@@ -36,4 +36,4 @@ class MassUnit(object):
     def unit_kg(self):
         """Return the value of one Kilogram (kg)
         based on a base metric value"""
-        return self.metric_base * 1000000.0
+        return self.metric_base * 1000.0
